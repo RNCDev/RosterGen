c9316560706c69be523e6bad38eeb63b60b6db09
RNCDev <RNCDev@users.noreply.github.com>
2024-12-26 23:03:18 -0500

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,6 +1,6 @@
 // app/page.js
 'use client';
-import RosterGenerator from '@/components/RosterGenerator'; // Correct absolute import
+import RosterGenerator from 'components/RosterGenerator'; // Correct absolute import
 
 export default function Home() {
   return <RosterGenerator />;
