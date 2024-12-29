4a7f428c20db99a83ff8d6adbdcb31dd146f7061
RNCDev <RNCDev@users.noreply.github.com>
2024-12-28 22:47:44 -0500

    reset structure

--- get_dir_structure.ps1
+++ get_dir_structure.ps1
@@ -1,8 +1,8 @@
 # Get the current directory
 $currentDirectory = Get-Location
 
-# Get all files recursively, including hidden and system files
-Get-ChildItem -Path $currentDirectory -Recurse -Force | ForEach-Object {
+# Get all files and directories recursively, excluding the .git directory and its contents
+Get-ChildItem -Path $currentDirectory -Recurse -Force | Where-Object { $_.FullName -notmatch "\\.git\\?" } | ForEach-Object {
     # Construct the relative path starting from the current directory
     $_.FullName.Substring($currentDirectory.Path.Length + 1) 
 } | Out-File -FilePath "dir_structure.txt"
