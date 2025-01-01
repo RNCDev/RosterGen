376b38d2a69ebb40858d4adb1cec39b8093cd5a8
RNCDev <RNCDev@users.noreply.github.com>
2025-01-01 01:13:42 -0500

    Update route.ts

--- src/app/api/groups/route.ts
+++ src/app/api/groups/route.ts
@@ -1,6 +1,7 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 import { sql } from '@vercel/postgres';
+import { PlayerInput } from '@/lib/db';
 
 interface CreateGroupRequest {
     groupCode: string;
